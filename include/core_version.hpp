f89cc780462fc33f0e7f021a3b43f9090728f3bf
Youriy Volkov <atribolt@gmail.com>
2024-02-16 12:16:16 +0300

    Разработка
    - поправил схему версионирования

---
+++ include/core_version.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string_view>
+
+namespace core
+{
+  std::string_view version();
+}
---
+++ src/core_version.cpp
@@ -0,0 +1,8 @@
+#include "core_version.hpp"
+
+#include "detector_core_version.hpp"
+
+std::string_view core::version()
+{
+  return DETECTOR_CORE_VERSION;
+}
